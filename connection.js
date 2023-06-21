69b7d5d1eb32a7fd4dfa4924093d2abc933cfb6a
Will Riffe <w_riffe@protonmail.com>
2023-06-21 12:33:20 -0400

    🔨 Constructed files connection, env, gitignore

---
+++ connection.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql2');
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+var dbConnect = function() {
+  return new Promise(function(resolve, reject) {
+    connection.connect(function(err) {
+      if (err) {
+        console.error('Error connecting to the database: ' + err.stack);
+        reject(err);
+        return;
+      }
+      console.log('Connected to the database.');
+      resolve();
+    });
+  });
+};
+
+// Exporting the connected promise-based connection
+module.exports = {
+    dbConnect,
+    connection
+};
