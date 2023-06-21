b7903c76fea2faf703fc311dcd72bbbb1b88e304
Will Riffe <w_riffe@protonmail.com>
2023-06-21 14:46:53 -0400

    🔧 Developed root Index and connection

--- connection.js
+++ connection.js
@@ -1,31 +1,30 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 
-// est. connection* to db
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
-const dbConnect = function() {
-  return new Promise(function(resolve, reject) {
-    connection.connect(function(err) {
-      if (err) {
-        console.error('Error connecting to the database: ' + err.stack);
-        reject(err);
-        return;
-      }
-      console.log('Connected to the database.');
-      resolve();
+
+const dbConnect = () => {
+    return new Promise((resolve, reject) => {
+        connection.connect((err) => {
+            if (err) {
+                console.error('Error connecting to the database: ' + err.stack);
+                reject(err);
+                return;
+            }
+            console.log('Connected to the database.');
+            resolve();
+        });
     });
-  });
 };
 
-// Exporting the connected promise-based connection
 module.exports = {
-    dbConnect,
-    connection
+    connection,
+    dbConnect
 };
--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const inquirer = require("inquirer");
-const { connection, dbconnect } = require("./connection");
-const db = require("./db");
+const { connection, dbConnect } = require("./connection");
+const fs = require("fs");
 
 
 
-async function connectToDatabase() {
+async function dbConnect() {
     try {
-        await dbconnection();
+        await connection.connect();
+
+        console.log('Database connected.');
+
+        const schema = fs.readFileSync('schema.sql', 'utf8');
+        await connection.query(schema);
+
+        console.log('Established Schema');
+
+        const seeds = fs.readFileSync('seeds.sql', 'utf8');
+        await connection.query(seeds);
+
+        console.log('Spread Seeds');
+
+
         connectCorrect();
+
+    
     } catch (error) {
         console.error('CONNECTION ERROR:', error);
     }
 }
-connectToDatabase();
+
+
+
+dbConnect();
 
 
 
