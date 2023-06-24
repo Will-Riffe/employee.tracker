3dfd6d24b539f5734423f68145fbe27eaaaa18c8
Will Riffe <w_riffe@protonmail.com>
2023-06-24 17:31:12 -0400

    🚚 Updated db index after merge issues

--- db/index.js
+++ db/index.js
@@ -1,11 +1,32 @@
-const connection = require('../connection')
+const connection = require("../config/connection.js");
 
 class DB {
-    constructor() {
-      this.connection = connection;
-    }
+  constructor() {
+    this.connection = connection;
+  }
 
-  findAllEmployees() {
+
+
+  getDepartments() {
+    const query = "SELECT department.id, department.name FROM department;";
+    return this.connection.promise().query(query);
+  }
+
+  getRoles() {
+    const query = `
+          SELECT
+            role.id,
+            role.title,
+            department.name AS department,
+            role.salary
+          FROM
+            role
+            LEFT JOIN department ON role.department_id = department.id;
+        `;
+    return this.connection.promise().query(query);
+  }
+
+  getEmployees() {
     const query = `
       SELECT
         employee.id,
@@ -24,6 +45,16 @@ class DB {
     return this.connection.promise().query(query);
   }
 
+  addDept(department) {
+    const query = "INSERT INTO department SET ?";
+    return this.connection.promise().query(query, department);
+  }
+
+  roleAdder(role) {
+    const query = "INSERT INTO role SET ?";
+    return this.connection.promise().query(query, role);
+  }
+
   findAllPossibleManagers(employeeId) {
     const query = `
       SELECT
@@ -38,7 +69,7 @@ class DB {
     return this.connection.promise().query(query, employeeId);
   }
 
-  createEmployee(employee) {
+  employeeAdd(employee) {
     const query = "INSERT INTO employee SET ?";
     return this.connection.promise().query(query, employee);
   }
@@ -53,102 +84,6 @@ class DB {
     return this.connection.promise().query(query, [roleId, employeeId]);
   }
 
-  updateEmployeeManager(employeeId, managerId) {
-    const query = "UPDATE employee SET manager_id = ? WHERE id = ?";
-    return this.connection.promise().query(query, [managerId, employeeId]);
-  }
-
-  findAllRoles() {
-    const query = `
-      SELECT
-        role.id,
-        role.title,
-        department.name AS department,
-        role.salary
-      FROM
-        role
-        LEFT JOIN department ON role.department_id = department.id;
-    `;
-    return this.connection.promise().query(query);
-  }
-
-  createRole(role) {
-    const query = "INSERT INTO role SET ?";
-    return this.connection.promise().query(query, role);
-  }
-
-  removeRole(roleId) {
-    const query = "DELETE FROM role WHERE id = ?";
-    return this.connection.promise().query(query, roleId);
-  }
-
-  findAllDepartments() {
-    const query = "SELECT department.id, department.name FROM department;";
-    return this.connection.promise().query(query);
-  }
-
-  viewDepartmentBudgets() {
-    const query = `
-      SELECT
-        department.id,
-        department.name,
-        SUM(role.salary) AS utilized_budget
-      FROM
-        employee
-        LEFT JOIN role ON employee.role_id = role.id
-        LEFT JOIN department ON role.department_id = department.id
-      GROUP BY
-        department.id,
-        department.name;
-    `;
-    return this.connection.promise().query(query);
-  }
-
-  createDepartment(department) {
-    const query = "INSERT INTO department SET ?";
-    return this.connection.promise().query(query, department);
-  }
-
-  removeDepartment(departmentId) {
-    const query = "DELETE FROM department WHERE id = ?";
-    return this.connection.promise().query(query, departmentId);
-  }
-
-  findAllEmployeesByDepartment(departmentId) {
-    const query = `
-      SELECT
-        employee.id,
-        employee.forename,
-        employee.surname,
-        role.title
-      FROM
-        employee
-        LEFT JOIN role ON employee.role_id = role.id
-        LEFT JOIN department department ON role.department_id = department.id
-      WHERE
-        department.id = ?;
-    `;
-    return this.connection.promise().query(query, departmentId);
-  }
-
-  findAllEmployeesByManager(managerId) {
-    const query = `
-      SELECT
-        employee.id,
-        employee.forename,
-        employee.surname,
-        department.name AS department,
-        role.title
-      FROM
-        employee
-        LEFT JOIN role ON role.id = employee.role_id
-        LEFT JOIN department ON department.id = role.department_id
-      WHERE
-        manager_id = ?;
-    `;
-    return this.connection.promise().query(query, managerId);
-  }
-  
 }
 
 
